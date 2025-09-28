0ecafac677a88d1509edcc8588e3ee2a4f85b32a
Sabrina Villano <villanosabrina123@gmail.com>
2025-09-27 21:35:12 -0300

    Agrego login con validación por usuario o email

--- app/src/main/java/com/example/pokedex_repo/Login_Activity.kt
+++ app/src/main/java/com/example/pokedex_repo/Login_Activity.kt
@@ -8,6 +8,8 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class Login_Activity : AppCompatActivity() {
 
@@ -37,13 +39,48 @@ class Login_Activity : AppCompatActivity() {
 
         // Botón Iniciar sesión
         btnLogin.setOnClickListener {
-            Toast.makeText(this, "Iniciar Sesión", Toast.LENGTH_SHORT).show()
+            val usuarioOEmail = textEmail.text.toString().trim()
+            val password = textPassword.text.toString().trim()
 
-            // Pasa directo a MainActivity sin validar por ahora
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish() // cierra el login para que no vuelva atras
-
+            if (usuarioOEmail.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Completa todos los campos", Toast.LENGTH_SHORT).show()
+            } else {
+                if (validarUsuario(usuarioOEmail, password)) {
+                    Toast.makeText(this, "Login exitoso", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this, "Error datos incorrectos", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
+    }
+
+    private fun validarUsuario(usuarioOEmail: String, password: String): Boolean {
+        try {
+            val fis = openFileInput("usuarios.txt")
+            val reader = BufferedReader(InputStreamReader(fis))
+            var line: String?
+
+            while (reader.readLine().also { line = it } != null) {
+                val datos = line!!.split(";")
+                if (datos.size == 3) {
+                    val nicknameGuardado = datos[0]
+                    val mailGuardado = datos[1]
+                    val passGuardada = datos[2]
+
+                    // Esto valida tanto por nickname como por email
+                    if ((mailGuardado == usuarioOEmail || nicknameGuardado == usuarioOEmail) && passGuardada == password) {
+                        reader.close()
+                        return true
+                    }
+                }
+            }
+            reader.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return false
     }
 }
